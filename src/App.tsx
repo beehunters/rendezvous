e04b0775bdeef2232c93243744e5226d9e16d627
Beemhuse <brightawah94@gmail.com>
2024-09-20 11:16:58 +0100

    fix: route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import ErrorPage from "./pages/ErrorPage";
+// import ErrorPage from "./pages/ErrorPage";
 import Home from "./pages/Home";
 import HomeLayout from "./layout/HomeLayout";
 import EventDetail from "./pages/eventDetail/EventDetail";
@@ -13,9 +13,9 @@ function App() {
         <Route path="/" element={<HomeLayout />}>
           <Route index element={<Home />} /> {/* Default route */}
           <Route path="event" element={<EventDetail />} /> 
-          <Route path="*" element={<NotFound />} /> {/* Catch-all for 404 */}
         </Route>
-        <Route path="*" element={<ErrorPage />} /> {/* Global error page */}
+          <Route path="*" element={<NotFound />} /> 
+        {/* <Route path="*" element={<ErrorPage />} /> Global error page */}
       </Routes>
     </Router>
   );
