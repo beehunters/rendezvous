fe395610be5cda6683514c0263eca571bac412e0
Beemhuse <brightawah94@gmail.com>
2024-09-20 11:27:00 +0100

    fix: routes

--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -45,7 +45,7 @@ export default function EventList({ events, isLoading }: EventListItemProps) {
                     date={event.date}
                     time={event.time}
                     description={event.description}
-                    link={`/event/?id=${event.id}`}
+                    link={`event/?id=${event.id}`}
                   />
                 </Grid>
               )
